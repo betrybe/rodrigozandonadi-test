08ca6b76a8180255eb5af9d4cda20c7df53ce6f1
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2021-09-29 10:32:09 -0300

    Correção da string de conexão com o BD

--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://mongodb:27017/Cookmaster')
-.then((conn) => console.log(`API conectada ao banco de dados MongoDB:${conn}`))
+mongoose.connect('mongodb://127.0.0.1:27017/Cookmaster')
+.then(() => console.log('API conectada ao banco de dados MongoDB'))
 .catch((error) => console.error(`Não foi possível conectar ao bando de dados:${error}`));
 
 const db = mongoose.connection;
