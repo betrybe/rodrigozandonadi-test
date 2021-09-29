9d82f2f40ddb6f6b94b2a2aa03e3a6abd409d865
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2021-09-29 18:33:27 -0300

    Refactory users.controllers.js, add post user

--- src/app/controllers/users.controllers.js
+++ src/app/controllers/users.controllers.js
@@ -1,26 +1,41 @@
 import db from '../models/users.models';
 
+const { body, validationResult } = require('express-validator');
+
 const Users = db.Mongoose.model('users', db.UserSchema, 'users');
 
 // eslint-disable-next-line max-lines-per-function
 module.exports = ((app) => {
-    app.route('/users')
+    app.route('/users')        
     .get(async (req, res, next) => {
         await Users.find({}).lean().exec()
         .then((data) => res.status(200).json({ users: data }))
-        .catch((erro) => res.status(500).json({ message: 'Deu ruim' }));
+        .catch((erro) => {
+            res.status(500).json({ message: 'Deu ruim' });
+            console.log(erro);
+        });
         next();
     })
-    .post(async (req, res, next) => {
-        let users = new Users({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            role: 'user',
+    // eslint-disable-next-line max-lines-per-function
+    .post(
+        body('name').notEmpty().withMessage('O campo nome é obrigatório'),
+        body('email').notEmpty().withMessage('O campo email é obrigatório'),
+        body('email').isEmail().withMessage('O email digitado não é válido.'),
+        body('password').notEmpty().withMessage('O campo password é obrigatório'),
+        async (req, res, next) => {
+            let { name, email, password } = req.body;
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            let users = new Users({ name, email, password });
+            await users.save()
+                .then((data) => res.status(200).json({ users: data }))
+                .catch((erro) => {
+                    res.status(500).json({ message: 'Deu ruim' });
+                    console.log(erro);
+                })
+            ; 
+            next();
         });
-        await users.save()
-        .then((data) => res.status(200).json({ users: data }))
-        .catch((erro) => res.status(500).json({ message: 'Deu ruim' }));
-        next();
-    });
 });
